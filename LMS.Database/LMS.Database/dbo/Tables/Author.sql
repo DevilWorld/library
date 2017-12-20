0fb61c504e24d5dc85e58abd4cc1e6fcd1db9448
Dinesh Raj Venkatachalam <dinesh.venkatachalam@state.ma.us>
2017-12-20 16:41:49 -0500

    SQL Script for author table creation.

---
+++ LMS.Database/LMS.Database/dbo/Tables/Author.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Author] (
+    [AuthorId]     INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName]    VARCHAR (50) NOT NULL,
+    [MiddleName]   VARCHAR (50) NULL,
+    [LastName]     VARCHAR (50) NOT NULL,
+    [CreatedBy]    VARCHAR (30) NOT NULL,
+    [CreatedDate]  DATETIME     NOT NULL,
+    [ModifiedBy]   VARCHAR (30) NULL,
+    [ModifiedDate] DATETIME     NULL,
+    [Active]       BIT          NOT NULL,
+    CONSTRAINT [pk_author_authorId] PRIMARY KEY CLUSTERED ([AuthorId] ASC)
+);
+
